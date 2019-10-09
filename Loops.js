7c00915aceaa944a9a7604562266888b469bb8ba
KaitlynHoudek <54539694+KaitlynHoudek@users.noreply.github.com>
2019-10-09 08:54:40 -0400

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,82 @@
+
+var i = 1;
+
+while(i < 20){
+  console.log("Hello, world");
+  i++;
+}
+
+var moviesArray = ["Casablanca", "Star Wars", "Singing in the Rain", "The Wizard of Oz", "Die Hard", "Nightmare before Christmas"];
+
+var i = 0; 
+
+while(i < moviesArray.length){
+  console.log(moviesArray[i]);
+  i++;
+}
+
+
+
+for(var i = 0; i < moviesArray.length; i++){
+    console.log(moviesArray[i]);
+  }
+
+  
+var i = 1;
+
+while(i <100){
+  console.log("QUACK, QUACK");
+  i++;
+}
+
+var foodArray = ["Chicken Nuggets", "Tacos", "Oreos", "Corn", "Turkey Sandwich", "Pizza"];
+
+var i = 0; 
+
+while(i < foodArray.length){
+  console.log(foodArray[i]);
+  i++;
+}
+
+var cohortOneStudents = ["Sable","David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"];
+
+var i = 0; 
+
+while(i <cohortOneStudents.length){
+    console.log(cohortOneStudents[i]);
+    i++;
+  }
+
+  var interestsArray = ["One of my interests is: drawing","One of my interests is: sleeping", "One of my interests is: reading", "One of my interests is: Bob Ross", "One of my interests is: makeup" ];
+
+  var i = 0; 
+
+  while(i <interestsArray.length){
+    console.log([interestsArray]);
+    i++;
+  }
+
+  for (i = 0; < grades.length i++) (
+if(grade[i] >=70 && grades[i] <= 76) {
+    console.log(grades[i], "You got a D")
+if(grade[i] >=77 && grades[i] <= 84) {
+    console.log(grades[i], "You got a C")
+if(grade[i] >=85 && grades[i] <= 92) {
+    console.log(grades[i], "You got a B")
+if(grade[i] >=93 && grades[i] <= 100) {
+    console.log(grades[i], "You got a A")
+
+  while(i <10){
+    console.log("Harry Potter and the Sorcerer's Stone");
+    i++;
+  }
+
+  while(i <10){
+    console.log("Harry Potter and the Chamber of Secrets");
+    i++;
+  }
+
+  while(i <10){
+    console.log("Harry Potter and the Deathly Hallows");
+    i++;
+  }
