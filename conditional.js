17664720398e663a0b5dfe1c5002f46f116780d7
KaitlynHoudek <54539694+KaitlynHoudek@users.noreply.github.com>
2019-10-09 08:58:11 -0400

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,60 @@
+var likestoTravel = true;
+
+if(likestoTravel=== true){
+    console.log("Bon voyage!");
+  } else {
+    console.log("Enjoy your couch!")
+  }
+  
+  var fullMoon = true;
+  var isWolf = false;
+
+  if(fullMoon && isWolf){
+    console.log("AWOOOOOOOOOOO");
+  }  
+if(fullMoon || isWolf){
+    console.log("AWOOOOOOOOOOO");
+}
+
+var favoriteFood = "sour candy";
+
+if(favoriteFood === "fried pickles"){
+    console.log("Congratulations! You have excellent taste!");
+  } else {
+    console.log("Clearly you have not tried fired pickles.")
+  }
+  
+var likesDogs = true;
+
+if(likesDogs=== true){
+    console.log("You're a dog person!");
+  } else {
+    console.log("You're a cat person!")
+  }
+  
+  var birthYear = 2000;
+
+  if(birthYear=== 2000){
+    console.log("Cool!");
+  } else {
+    console.log("What year were you born?")
+  }
+  
+
+var time = "12:00 PM";
+var isHungry = true;
+
+if(time=== "7:30 AM" && isHungry){
+    console.log("Time for breakfast!");
+  } else if (time === "12:00 PM"){
+    console.log("Time for lunch!");
+  }
+  
+var grade = 100;
+
+
+if(grade=== 100){
+    console.log("You got an A!");
+  } else {
+    console.log("You got an F! Booooo Hooooo!");
+  }
